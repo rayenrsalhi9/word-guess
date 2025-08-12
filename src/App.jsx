c3eb247ce288a999a3349e3e55c1093a1d0693c2
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-12 18:16:26 +0100

    initial commit.

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react"
+import { languages } from "./languages"
+
+
+export default function App() {
+
+    const [currentWord, setCurrentWord] = useState('react')
+
+    const languageEl = languages.map((el, index) => {
+        const styles = {
+            backgroundColor: el.backgroundColor,
+            color: el.color
+        }
+        return (
+            <span 
+                key={index} 
+                style={styles}
+                className="language"
+            >
+                {el.name}
+            </span>
+        )
+    })
+
+    const wordLetters = currentWord.split('').map((el, index) => {
+        return <span key={index}>{el}</span>
+    })
+
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    const keyboard = alphabet.split('').map(el => <button key={el}>{el}</button>)
+
+    return (
+        <main className="container">
+            
+            <header>
+                <h1>Assembly: Endgame</h1>
+                <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+
+            <section className="status">
+                <h2>You win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+
+            <section className="languages">{ languageEl }</section>
+
+            <section className="letters">{ wordLetters }</section>
+
+            <section className="keyboard">{ keyboard }</section>
+
+            <button className="new-game">New Game</button>
+        </main>
+    )
+}
